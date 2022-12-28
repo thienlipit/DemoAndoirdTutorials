e5729831e9a3cc065214774c61bd89e92a31b523
thienlipit <tranthienrs24@gmail.com>
2022-12-28 15:06:01 +0700

    add project BLEexample

--- BLEexample/app/src/main/java/com/example/bleexample/MainActivity.kt
+++ BLEexample/app/src/main/java/com/example/bleexample/MainActivity.kt
@@ -1,11 +1,173 @@
 package com.example.bleexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.Observer
+import org.altbeacon.beacon.*
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
+    val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                Log.d("location 1", "Precise location access granted")
+                // Precise location access granted.
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // Only approximate location access granted.
+                Log.d("location 2", "Only approximate location access granted")
+            } else -> {
+            // No location access granted.
+            Log.d("location 3", "No location access granted")
+        }
+        }
+    }
+private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val beaconManager =  BeaconManager.getInstanceForApplication(this)
+        val region = Region("all-beacons-region", null, null, null)
+        // Set up a Live Data observer so this Activity can get monitoring callbacks
+        // observer will be called each time the monitored regionState changes (inside vs. outside region)
+        beaconManager.getRegionViewModel(region).regionState.observe(this, monitoringObserver)
+        beaconManager.startMonitoring(region)
+
+        // Before you perform the actual permission request, check whether your app
+        // already has the permissions, and whether your app needs to show a permission
+        // rationale dialog. For more details, see Request permissions.
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_SCAN,))
+
+        val btnBt = findViewById<Button>(R.id.BtBtn)
+        val btnBtOff = findViewById<Button>(R.id.BtBtn1)
+        val tvBt = findViewById<TextView>(R.id.BtTv)
+
+        // Declaring Bluetooth adapter
+        // On button Click
+        if(bluetoothAdapter.isEnabled){
+            tvBt.text = "Bluetooth is on"
+        } else {
+            tvBt.text = "Bluetooth is off"
+        }
+
+        btnBt.setOnClickListener {
+
+            // Enable or disable the Bluetooth and display
+            // the state in Text View
+            if (bluetoothAdapter.isEnabled) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return@setOnClickListener
+                }
+                bluetoothAdapter.disable()
+                tvBt.text = "Bluetooth is OFF"
+            } else {
+                bluetoothAdapter.enable()
+                tvBt.text = "Bluetooth is ON"
+            }
+        }
+
+        btnBtOff.setOnClickListener {
+            bluetoothAdapter.disable()
+            tvBt.text = "Bluetooth is OFF now"
+        }
+//        scanLeDevice()
+    }
+
+
+    private val monitoringObserver = Observer<Int> { state ->
+        if (state == MonitorNotifier.INSIDE) {
+            Log.d("TAG", "Detected beacons(s)")
+        }
+        else {
+            Log.d("TAG", "Stopped detecteing beacons")
+        }
+    }
+
+
+
+    private val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+    private var scanning = false
+    private val handler = Handler()
+
+    // Stops scanning after 10 seconds.
+    private val SCAN_PERIOD: Long = 10000
+
+    @SuppressLint("MissingPermission")
+    private fun scanLeDevice() {
+        Log.d("run ", "run here")
+        if (!scanning) { // Stops scanning after a pre-defined scan period.
+            handler.postDelayed({
+                scanning = false
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_SCAN
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return@postDelayed
+                }
+                bluetoothLeScanner?.stopScan(leScanCallback)
+            }, SCAN_PERIOD)
+            scanning = true
+            bluetoothLeScanner?.startScan(leScanCallback)
+        } else {
+            scanning = false
+            bluetoothLeScanner?.stopScan(leScanCallback)
+        }
+    }
+
+    // Device scan callback.
+    private val leScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            Log.d("result", result.device.toString())
+//            leDeviceListAdapter.addDevice(result.device)
+//            leDeviceListAdapter.notifyDataSetChanged()
+        }
     }
 }
