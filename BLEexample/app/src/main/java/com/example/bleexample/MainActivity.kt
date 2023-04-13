1dd801bca9ebdcf5e4be37a939eb4e589f672bcd
Tran Minh Thien <tranthienrs24@gmail.com>
2023-04-13 21:19:35 +0700

    edit some code

--- BLEexample/app/src/main/java/com/example/bleexample/MainActivity.kt
+++ BLEexample/app/src/main/java/com/example/bleexample/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.bleexample
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.os.ParcelUuid
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
@@ -17,85 +20,58 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.util.keyIterator
+import com.example.bleexample.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.math.pow
 
 const val MAC_BEACON_abc: String = "D5:43:8F:F6:0D:46"
 const val MAC_BEACON_01: String = "D5:D2:C0:EC:BA:76"
 const val MAC_BEACON_02: String = "DE:79:8D:06:0C:AC"
+const val MAC_BEACON_PIXEL: String = "60:CC:D6:78:D2:47"
+const val FEASY_BEACON: String = "DC:0D:30:14:32:6B"
 open class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
          val mBluetoothAdapter =
             (applicationContext!!.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        RequestPermission(this).requestPermission()
+        RequestPermission(this).requestAllPermission()
+        btnLocationClick(binding.btnLocation)
+        btnSensorClick(binding.sensor)
+        binding.BtTv.text = if(mBluetoothAdapter.isEnabled) getString(R.string.bluetooth_on) else getString(R.string.bluetooth_off)
+        btnBTOnClick(binding.btnBTOn, mBluetoothAdapter, binding.BtTv)
+        btnBTOffClick(binding.btnBTOff, mBluetoothAdapter, binding.BtTv)
 
-        val btnBtOn = findViewById<Button>(R.id.BtBtn)
-        val btnBtOff = findViewById<Button>(R.id.BtBtn1)
-        val btnSensor = findViewById<Button>(R.id.sensor)
-        val btnLocation = findViewById<Button>(R.id.btnLocation)
-        val tvBt = findViewById<TextView>(R.id.BtTv)
+        val bluetoothAdapter =
+            (applicationContext.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter
+        btnScan(binding.btnScan, bluetoothAdapter)
 
-        btnLocation.setOnClickListener {
-            val intent = Intent(this, MyNavigationService::class.java)
-            startActivity(intent)
-        }
+        btnStop(binding.btnStop, bluetoothAdapter)
 
-        btnSensor.setOnClickListener {
-            val intent = Intent(this, SensorActivity::class.java)
-            startActivity(intent)
-        }
 
-        if(mBluetoothAdapter.isEnabled){
-            tvBt.text = getString(R.string.bluetooth_on)
-        } else {
-            tvBt.text = getString(R.string.bluetooth_off)
-        }
-
-        btnBtOn.setOnClickListener {
-            if (mBluetoothAdapter.isEnabled) {
-                if (ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.BLUETOOTH_CONNECT
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    return@setOnClickListener
-                }
-                mBluetoothAdapter.disable()
-                tvBt.text = getString(R.string.bluetooth_off)
-            } else {
-                mBluetoothAdapter.enable()
-                tvBt.text = getString(R.string.bluetooth_on)
-            }
-        }
-
-        btnBtOff.setOnClickListener {
-            mBluetoothAdapter.disable()
-            tvBt.text = getString(R.string.bluetooth_off)
-        }
-        scanLeDevice()
-
-        TestBLEManager().demoBLE(this, this)
+//        scanLeDevice()
+//        TestBLEManager().demoBLE(this, this)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("MissingPermission")
     private fun scanLeDevice() {
         val bluetoothAdapter =
             (applicationContext.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter
 
         val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+
         var scanning = false
         val handler = Handler()
 
         // Stops scanning after 10 seconds.
-        val scanPeriod = 10000L
+        val scanPeriod = 30000L
 
-        Log.d("run ", "run here")
         if (!scanning) { // Stops scanning after a pre-defined scan period.
             handler.postDelayed({
                 scanning = false
@@ -103,16 +79,8 @@ open class MainActivity : AppCompatActivity() {
                         this,
                         Manifest.permission.BLUETOOTH_SCAN
                     ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return@postDelayed
-                }
+                ) return@postDelayed
+
                 bluetoothLeScanner?.stopScan(leScanCallback)
             }, scanPeriod)
             scanning = true
@@ -131,38 +99,29 @@ open class MainActivity : AppCompatActivity() {
         @RequiresApi(Build.VERSION_CODES.O)
         override fun onScanResult(callbackType: Int, result: ScanResult) {
             super.onScanResult(callbackType, result)
-            if(result.device.toString() == MAC_BEACON_01){
-//                val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
-                val sdf = SimpleDateFormat("hh:mm:ss")
-                val currentDate = sdf.format(Date())
 
-//                Log.d("all info", result.toString())
-//                Log.d("device", result.device.toString())
-                Log.d("rssi", result.rssi.toString())
-//                Log.d("advertisingSid", result.advertisingSid.toString())
-//                Log.d("txPower", result.txPower.toString())
-//                Log.d("txPowerLevel", result.scanRecord!!.txPowerLevel.toString())
+            if(result.device.toString() == FEASY_BEACON){
+                Log.d("ScanResult", result.scanRecord.toString())
 
-                val base = 10
+                val SERVICE_UUID = "0000FEAA-0000-1000-8000-00805F9B34FB"
+                val scanRecord = result.scanRecord
+//                val serviceData = scanRecord.getServiceData( UUID.fromString(SERVICE_UUID))
+
+
+/*                val base = 10
                 val n = 3   // environment index
-
                 val measurePower = -46.0
                 val txPower = 6
                 val advInterval = 300  //ADV_interval 300ms
                 val realDistance = 1.0    // 1 meter
-
-
                 val exponent = ((measurePower - (result.rssi).toDouble())/(10*n))
                 val resultDistance = base.toDouble().pow(exponent)
                 Log.d("Distance", resultDistance.toString())
                 arrayDistance.add(resultDistance)
                 mutableListRSSI.add(result.rssi)
-
-
                 var avgDistance = 0.0
                 var avgRSSI = 0.0
                 if(arrayDistance.size == 3){
-
                     for (i in arrayDistance){
                         avgDistance += i
                     }
@@ -174,34 +133,41 @@ open class MainActivity : AppCompatActivity() {
                     Log.d("AVGDistance: ", avgDistance.toString())
                     arrayDistance.clear()
                     mutableListRSSI.clear()
-
                     val writeText = currentDate + "\t" + txPower + "\t" + advInterval + "\t" + measurePower + "\t" + n + "\t" + String.format("%.3f", avgDistance) + "\t" + realDistance + "\t" + String.format("%.3f", avgRSSI) + "\n"
                     StoreData().writeDataToFile("SS_Data.txt", writeText, applicationContext)
+                }*/
 
-                }
 
                 var manufacturerId = 0
                 result.scanRecord!!.manufacturerSpecificData.keyIterator().forEach { key ->
                     manufacturerId = key
                 }
-                showBeaconInfo(result, manufacturerId)
+                showBeaconInfo(result,manufacturerId )
 
             }
         }
     }
 
     fun showBeaconInfo(result: ScanResult, manufacturerId: Int) {
-        val uuid = toHexString(result.scanRecord!!.getManufacturerSpecificData(manufacturerId)!!.copyOfRange(2, 18))
-        Log.d("UUID", uuid)
 
-        val major = toHexString(result.scanRecord!!.getManufacturerSpecificData(manufacturerId)!!.copyOfRange(18, 20))
-        Log.d("MAJOR", major.toInt(16).toString())
+//        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val sdf = SimpleDateFormat("hh:mm:ss")
+        val currentDate = sdf.format(Date())
+        val manufacturerSpecificData = result.scanRecord!!.manufacturerSpecificData
+        if(manufacturerSpecificData != null) {
+            val uuid = toHexString(result.scanRecord!!.getManufacturerSpecificData(manufacturerId)!!.copyOfRange(2, 18))
+            Log.d("UUID", uuid)
 
-        val minor = toHexString(result.scanRecord!!.getManufacturerSpecificData(manufacturerId)!!.copyOfRange(20, 22))
-        Log.d("MINOR", minor.toInt(16).toString())
+            val major = toHexString(result.scanRecord!!.getManufacturerSpecificData(manufacturerId)!!.copyOfRange(18, 20))
+            Log.d("MAJOR", major.toInt(16).toString())
 
-        val txPower = toHexString(result.scanRecord!!.getManufacturerSpecificData(manufacturerId)!!.copyOfRange(22, 23))
-        Log.d("txPower", txPower.toInt(16).toString())
+            val minor = toHexString(result.scanRecord!!.getManufacturerSpecificData(manufacturerId)!!.copyOfRange(20, 22))
+            Log.d("MINOR", minor.toInt(16).toString())
+
+            val txPower = toHexString(result.scanRecord!!.getManufacturerSpecificData(manufacturerId)!!.copyOfRange(22, 23))
+            Log.d("txPower", txPower.toInt(16).toString())
+        } else return
+
     }
 
     private fun toHexString(bytes: ByteArray): String {
@@ -220,5 +186,131 @@ open class MainActivity : AppCompatActivity() {
     companion object {
         const val TAG = "Main"
         val HEX = "0123456789ABCDEF".toCharArray()
+    }
+
+    private fun btnLocationClick(btnLocation: Button){
+        btnLocation.setOnClickListener {
+            val intent = Intent(this, MyNavigationService::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun btnSensorClick(btnSensor: Button){
+        btnSensor.setOnClickListener {
+            val intent = Intent(this, SensorActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun btnBTOnClick(btnBtOn: Button, mBluetoothAdapter: BluetoothAdapter, tvBt: TextView){
+        btnBtOn.setOnClickListener {
+            if (mBluetoothAdapter.isEnabled) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) return@setOnClickListener
+                return@setOnClickListener
+            }
+            else {
+                mBluetoothAdapter.enable()
+                tvBt.text = getString(R.string.bluetooth_on)
+            }
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private fun btnBTOffClick(btnBtOff: Button, mBluetoothAdapter: BluetoothAdapter, tvBt: TextView){
+        btnBtOff.setOnClickListener {
+            mBluetoothAdapter.disable()
+            tvBt.text = getString(R.string.bluetooth_off)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun btnScan(btnScan: Button, bluetoothAdapter: BluetoothAdapter){
+        Log.d(javaClass.name, "Start scan")
+        btnScan.setOnClickListener {
+            val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+//            if (ActivityCompat.checkSelfPermission(
+//                    this,
+//                    Manifest.permission.BLUETOOTH_SCAN
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                return@setOnClickListener}
+            if(bluetoothAdapter.isLeCodedPhySupported){
+                bluetoothLeScanner.startScan(leScanCallback)
+            } else {
+                Log.d("isLeCodedPhySupported", "false")
+                bluetoothLeScanner.startScan(leScanCallback)
+            }
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun btnStop(btnStop: Button, bluetoothAdapter: BluetoothAdapter){
+        Log.d(javaClass.name, "Stop scan")
+        btnStop.setOnClickListener {
+            val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_SCAN
+                ) != PackageManager.PERMISSION_GRANTED
+            ) return@setOnClickListener
+            if(bluetoothAdapter.isLeCodedPhySupported){
+                bluetoothLeScanner.startScan(leScanCallback)
+            } else {
+                Log.d("isLeCodedPhySupported", "false")
+                bluetoothLeScanner.startScan(leScanCallback)
+            }
+        }
+    }
+
+    fun getAoaAoD(){
+        val service_uuid1 = "0000fef5-0000-1000-8000-00805f9b34fb"
+        val SERVICE_UUID = "0000FEAA-0000-1000-8000-00805F9B34FB"
+        val AOADATA_UUID = byteArrayOf(
+            0xBF.toByte(),
+            0x24.toByte(),
+            0x1F.toByte(),
+            0x16.toByte(),
+            0x80.toByte(),
+            0x02.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x09.toByte(),
+            0x00.toByte(),
+            0x03.toByte(),
+            0x19.toByte(),
+            0xC1.toByte()
+        )
+
+        val scanner = BluetoothAdapter.getDefaultAdapter().bluetoothLeScanner
+        val scanCallback: ScanCallback = object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
+                val scanRecord = result.scanRecord
+//                if (scanRecord != null && scanRecord.serviceUuids != null && scanRecord.serviceUuids.contains(
+                if (scanRecord != null && scanRecord.serviceUuids != null && scanRecord.serviceUuids.contains(
+                        ParcelUuid.fromString(SERVICE_UUID)
+                    )
+                ) {
+                    val serviceData = scanRecord.getServiceData(ParcelUuid.fromString(SERVICE_UUID))
+                    if (serviceData != null && Arrays.equals(
+                            Arrays.copyOfRange(serviceData, 0, 15),
+                            AOADATA_UUID
+                        )
+                    ) {
+                        val aoa = Arrays.copyOfRange(serviceData, 15, 17)
+                        val aod = Arrays.copyOfRange(serviceData, 17, 19)
+                        // Do something with the AoA and AoD information
+                        // ...
+                    }
+                }
+            }
+        }
+
+        val settings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+            .build()
+//        scanner.startScan(null, settings, scanCallback)
     }
 }
--- BLEexample/app/src/main/java/com/example/bleexample/RequestPermission.kt
+++ BLEexample/app/src/main/java/com/example/bleexample/RequestPermission.kt
@@ -3,7 +3,9 @@ package com.example.bleexample
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.bluetooth.BluetoothAdapter
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
@@ -11,34 +13,73 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class RequestPermission( appCompatActivity: AppCompatActivity) {
-    @RequiresApi(Build.VERSION_CODES.N)
-    val locationPermissionRequest = appCompatActivity.registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        when {
-            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                Log.d("location 1", "Precise location access granted")
-                // Precise location access granted.
+    private var requestBluetoothOn =
+        appCompatActivity.registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                //granted
+                Log.d("TAG", "Bluetooth is ON")
+            } else {
+                //deny
+                Log.d("TAG", "Bluetooth is OFF")
             }
-            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                // Only approximate location access granted.
-                Log.d("location 2", "Only approximate location access granted")
-            } else -> {
-            // No location access granted.
-            Log.d("location 3", "No location access granted")
         }
+    @RequiresApi(Build.VERSION_CODES.N)
+    val requestMultiplePermissionsAPI30Below =
+        appCompatActivity.registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                Log.d("List Permissions: ", "${it.key} = ${it.value}")
+            }
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, true) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, true) -> {
+                    // Only approximate location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.BLUETOOTH, true) -> {
+                    // Only approximate location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.BLUETOOTH_ADMIN, true) -> {
+                    // Only approximate location access granted.
+                }
+                else -> {
+                    // No location access granted.
+                }
+            }
         }
-    }
+    private val requestMultiplePermissionsAPI31Above =
+        appCompatActivity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                Log.d("List Permissions: ", "${it.key} = ${it.value}")
+            }
+        }
 
-    @SuppressLint("NewApi")
-    fun requestPermission (){
-        locationPermissionRequest.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.BLUETOOTH_ADMIN,
-            Manifest.permission.BLUETOOTH,
-            Manifest.permission.BLUETOOTH_CONNECT,
-            Manifest.permission.BLUETOOTH_SCAN,
-        ))
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun requestAllPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Log.d("SDK", ">31")
+            requestMultiplePermissionsAPI31Above.launch(
+                arrayOf(
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        } else {
+            Log.d("RUN ELSE", "<=30")
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            requestBluetoothOn.launch(enableBtIntent)
+            requestMultiplePermissionsAPI30Below.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
     }
 }
